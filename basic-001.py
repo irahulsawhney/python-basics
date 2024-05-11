eb373b29e81b7a2aa724b3977e3383c83679d863
Rahul Sawhney <rahul.sawhney.web@gmail.com>
2024-05-11 18:32:18 -0500

    .\basic-001.py

---
+++ basic-001.py
@@ -0,0 +1,16 @@
+# This is a single-line comment in Python.
+
+'''
+This is a
+multi-line comment in Python.
+'''
+"""
+This is another
+multi-line comment in Python.
+"""
+
+print("Hello, and welcome to the Python World!")
+
+if 5 > 2:
+    print("Five is indeed greater than two!") # this belongs to the if block
+print("End of program") # this does not belong to the if block
